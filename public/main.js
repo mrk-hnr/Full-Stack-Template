0adbecc0348b1c7edf3356c9722bb98a7691325c
mrk-hnr <mrk.hnr@hotmail.com>
2023-08-23 21:40:58 +0800

    Corrected .js and .css path on .ejs

--- public/main.js
+++ public/main.js
@@ -1,9 +1,27 @@
 //Example fetch using pokemonapi.co
+document.querySelector('button').addEventListener('click', getFetch)
+
+function getFetch(){
+  const choice = document.querySelector('input').value.toLowerCase()
+  const url = `https://pokeapi.co/api/v2/pokemon/${choice}`
+
+  fetch(url)
+      .then(res => res.json()) // parse response as JSON
+      .then(data => {
+        console.log(data)
+        document.querySelector('h2').innerText = data.name
+        document.querySelector("img").src = data.sprites.front_default
+      })
+      .catch(err => {
+          console.log(`error ${err}`)
+      });
+}
+
 // document.querySelector('button').addEventListener('click', getFetch)
 
 // function getFetch(){
 //   const choice = document.querySelector('input').value.toLowerCase()
-//   const url = `https://pokeapi.co/api/v2/pokemon/${choice}`
+//   const url = `https://api.nasa.gov/planetary/apod?api_key=zU71SV2z8UAS2tpSRxtx9Ii4giGUAk6QIufK4bCn`
 
 //   fetch(url)
 //       .then(res => res.json()) // parse response as JSON
@@ -14,20 +32,4 @@
 //           console.log(`error ${err}`)
 //       });
 // }
-
-document.querySelector('button').addEventListener('click', getFetch)
-
-function getFetch(){
-  const choice = document.querySelector('input').value.toLowerCase()
-  const url = `https://api.nasa.gov/planetary/apod?api_key=zU71SV2z8UAS2tpSRxtx9Ii4giGUAk6QIufK4bCn`
-
-  fetch(url)
-      .then(res => res.json()) // parse response as JSON
-      .then(data => {
-        console.log(data)
-      })
-      .catch(err => {
-          console.log(`error ${err}`)
-      });
-}
 
