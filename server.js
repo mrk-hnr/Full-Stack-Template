739da2a978d37f883cd7638d312a110394576b0b
mrk-hnr <mrk.hnr@hotmail.com>
2023-08-23 17:42:49 +0800

    Declared variables and connected to DB

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-// DEPENDENCIES SECTION START
+// STEP 2 - REQUIRING DEPENDENCIES
 
 const express = require("express")
 const app = express()
@@ -6,4 +6,18 @@ const cors = require("cors")
 const MongoClient = require("mongodb").MongoClient
 require("dotenv").config() // hides important details. create an env file and read data from it in our code. 
 
-// DEPENDENCIES SECTION END
+
+// STEP 3 - DECLARING VARIABLE
+let db,
+    dbConnectionString = process.env.DB_STRING, // grab variable from connection string from .env
+    dbName = "star-wars-quotes", // name of a db from mongo site
+    collection 
+
+// STEP 4 - CONNECTING TO MongoDB
+
+MongoClient.connect(dbConnectionString)
+    .then(client => {
+        console.log("Database Connection Status: Success!")
+        db = client.db(dbName)
+        collection = db.collection("quotes") // collection/lists under dbName from site.
+    })
